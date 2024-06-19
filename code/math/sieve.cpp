fbc62e25623cec16b987292bd4d62d7dd8ff95cb
MaxwellOliveira01 <maxwell1de1oliveira@gmail.com>
2024-06-18 22:30:31 -0300

    updated sieve

--- code/math/sieve.cpp
+++ code/math/sieve.cpp
@@ -1,17 +1,17 @@
-// nao "otimizado"
-
-vector<bool> sieve(int lim=1e5+17) {
-    vector<bool> isprime(lim+1, true);
-
-    isprime[0] = isprime[1] = false;
-    
-    for (int i = 2; i*i < lim; i++) {
-        if (isprime[i]) {
-            for (int j = i+i; j < lim; j += i) {
-                isprime[j] = false;
+vector<int> sieve(int MAXN){
+    //list of prime numbers up to MAXN
+    vector<int> primes;
+    bitset<(int)1e7> not_prime;
+    not_prime[0] = 1;
+    not_prime[1] = 1;
+    for(int i = 2; i <= MAXN; i++){
+        if(!not_prime[i]){
+            primes.push_back(i);
+            for(ll j = 1LL * i * i; j <= MAXN; j += i){
+                not_prime[(int)j] = 1;
             }
         }
     }
-
-    return isprime;
+    return primes;
 }
+
