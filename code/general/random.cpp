ff81a85b0a37e605520878aa0ae35ef2c7724a3c
MaxwellOliveira01 <maxwell1de1oliveira@gmail.com>
2024-06-18 22:30:46 -0300

    updated random.cpp

--- code/general/random.cpp
+++ code/general/random.cpp
@@ -1,6 +1,19 @@
-random_device dev;
-mt19937 rng(dev());
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    //mt19937 rng(chrono::steady_clock::now().time_since_epoch().count()); //gerar int
+    mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count()); //gerar ll
 
-uniform_int_distribution<mt19937::result_type> dist(1, 6); // distribution in range [1, 6]
+    /*usar rng() pra gerar numeros aleatórios.*/
+    /*usar rng() % x pra gerar numeros em [0, x-1]*/
+    for(int i = 0; i < 10; i++){
+        cout << rng() << endl;
+    }
+    vector<ll> arr = {1,2,3,4,5,6,7,8,9};
+    /*dá pra usar no shuffle de vector também*/
+    shuffle(arr.begin(), arr.end(),rng);
+    for(auto &x: arr) 
+        cout << x << endl;
 
-int val = dist(rng);
+}
