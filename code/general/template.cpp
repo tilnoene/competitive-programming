8a6428e7e6bb471d66a863f1b56525f8670de2b8
MaxwellOliveira01 <maxwell1de1oliveira@gmail.com>
2024-06-20 21:21:50 -0300

    improving template

--- code/general/template.cpp
+++ code/general/template.cpp
@@ -1,6 +1,21 @@
 #include <bits/stdc++.h>
+#define ff first
+#define ss second
 
 using namespace std;
+using ll = long long;
+using ld = long double;
+using pii = pair<int,int>;
+using vi = vector<int>;
+
+using tii = tuple<int,int,int>;
+// auto [a,b,c] = ...
+// .insert({a,b,c})
+
+const int oo = (int)1e9 + 5; //INF to INT
+const ll OO = 0x3f3f3f3f3f3f3f3fLL; //INF to LL
+
+// g++ -std=c++17 -Wall -Wshadow -fsanitize = address -O2 -o cod a.cpp
 
 int main() {
     ios::sync_with_stdio(false);
